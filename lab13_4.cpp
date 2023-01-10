3c6a33da62ad5f60cbf3d292a96d4a913e719702
kaewtar <kaewtarcmu@gmail.com>
2023-01-10 15:54:30 +0700

    done

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
+
+template<class T>
+void mySwap(T &, T &);
+
 
 int main(){
 	int x, y;
@@ -30,3 +34,10 @@ int main(){
 	return 0;
 }
 
+template<class T>
+void mySwap(T &x, T &y)
+{
+	 T z = x;
+	 x = y;
+	 y = z;
+}
